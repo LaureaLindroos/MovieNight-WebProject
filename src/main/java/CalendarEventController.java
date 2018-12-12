7f4a0f1e0c1371353b8d30b09c4d500cd7364fad
Laurea <laurea.lindroos@hotmail.com>
2018-12-12 12:05:55 +0100

    ...

--- src/main/java/CalendarEventController.java
+++ src/main/java/CalendarEventController.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public class CalendarEventController {
+
+    public static Event event () {
+        Instant now = LocalDateTime.of(2018, 12, 13, 10, 10).toInstant(ZoneOffset.UTC);
+        DateTime now2 = new DateTime(Date.from(now));
+
+        Instant end = LocalDateTime.of(2018, 12, 16,10, 10).toInstant(ZoneOffset.UTC);
+        DateTime end2 = new DateTime(Date.from(end));
+
+        Event event = new Event();
+
+        event.setStart(new EventDateTime().setDateTime(now2));
+        event.setEnd(new EventDateTime().setDateTime(end2));
+        event.setDescription("Middag");
+        event.setLocation("Malmö");
+
+        return event;
+    }
+
 }
