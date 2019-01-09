e84055a3184bf2b6f9c74bf348af5dda9c5294fe
Laurea <laurea.lindroos@hotmail.com>
2019-01-09 11:15:26 +0100

    Added methodcalls for finding movies in movie repository

--- src/main/java/movienights/repositories/MovieRepository.java
+++ src/main/java/movienights/repositories/MovieRepository.java
@@ -1,4 +1,14 @@
 package movienights.repositories;
 
-public class MovieRepository {
+import movienights.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+
+    Page<Movie> findAllByCategory(String category, org.springframework.data.domain.Pageable pageable);
+
+    Movie findById (int id);
 }
